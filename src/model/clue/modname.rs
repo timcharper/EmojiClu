884e0b8066428299357df6cea31c9b095437359a
Tim Harper <timcharper@gmail.com>
2025-01-25 14:56:53 -0700

    remove some cruft

--- src/model/clue/modname.rs
+++
@@ -1,7 +0,0 @@
-use crate::model::tile::Tile;
-
-#[derive(Clone, PartialEq, Eq)]
-pub struct TileAssertion {
-    pub tile: Tile,
-    pub assertion: bool, // true = positive assertion (tile exists), false = negative assertion (tile does not exist)
-}
